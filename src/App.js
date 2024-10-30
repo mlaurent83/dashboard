d80c85dd64e57b2e2e6fc23cd22219c381a99044
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-10-30 15:16:49 +0000

    added imports in navbar

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers,
-  Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor
+  Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor
 } from './pages'
 
 import './App.css';
 
 const App = () => {
-const activeMenu = false;
+const activeMenu = true;
 
   return (
     <div>
