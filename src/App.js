cc2ca6fbf58eef3169589f40e6efb642b794f600
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-10-30 14:30:33 +0000

    added tailwind styled navbar

--- src/App.js
+++ src/App.js
@@ -30,11 +30,13 @@ const activeMenu = false;
           </div>
         )}
         <div className={
-          activeMenu ? 'dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full'
-          :
-          'dark:bg-main-bg bg-main-bg min-h-screen w-full flex-2'
+          `dark:bg-main-bg bg-main-bg min-h-screen w-full ${ activeMenu ? 'md:ml-72'
+          :' flex-2'}`
         }>
-
+          <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg
+          navbar w-full'>
+            Navbar
+          </div>
         </div>
       </div>
       </BrowserRouter>
