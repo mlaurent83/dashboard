9e735c99d8d35eded310ca919d9de9e83c3794c3
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-01 15:18:59 +0000

    set up all imports for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,11 @@ import { FiShoppingCart } from 'react-icons/fi'
 import { BsChatLeft } from 'react-icons/bs'
 import { RiNotification3Line } from 'react-icons/ri'
 import { MdKeyboardArrowDown } from 'react-icons/md'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+
+import avatar from '../data/avatar.jpg'
+import { Cart, Chat, Notification, UserProfile } from '.'
+import { useStateContext } from '../contexts/ContextProvider'
 
 const Navbar = () => {
   return (
