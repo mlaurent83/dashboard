dc46f605fd981a5f74b101a847d28d57fd513316
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-01 16:00:00 +0000

    adding is clicked logic to navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor}) => (
 )
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu, handleClick } = useStateContext();
+  const { activeMenu, setActiveMenu, handleClick, isClicked, setIsClicked } = useStateContext();
 
   return (
     <div className='flex justify-between p-2 md:mx-6  relative'>
@@ -72,6 +72,11 @@ const Navbar = () => {
               <MdKeyboardArrowDown className='text-gray-400 text-14'/>
             </div>
           </TooltipComponent>
+
+          {isClicked.cart && <Cart/>}
+          {isClicked.chat && <Chat/>}
+          {isClicked.notification && <Notification/>}
+          {isClicked.userProfile && <UserProfile/>}
       </div>
     </div>
   )
