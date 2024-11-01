a83ae68ceadbf32f7a2430933d9995d5bdd2896a
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-01 15:42:51 +0000

    added clickable icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,13 +26,28 @@ const NavButton = ({ title, customFunc, icon, color, dotColor}) => (
 )
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, handleClick } = useStateContext();
 
   return (
     <div className='flex justify-between p-2 md:mx-6  relative'>
-      <NavButton title="Menu" customFunc={() =>
-        setActiveMenu((prevActiveMenu)=>
-          !prevActiveMenu)} color="blue" icon={<AiOutlineMenu />}/>
+      <NavButton title="Menu"
+          customFunc={() => setActiveMenu(
+          (prevActiveMenu)=> !prevActiveMenu)}
+          color="blue"
+          icon={<AiOutlineMenu />}/>
+
+      <div className='flex'>
+        <NavButton title="Cart"
+          customFunc={() => handleClick('cart')}
+          color="blue"
+          icon={<FiShoppingCart />}
+          />
+           <NavButton title="Chat"
+           dotColor='#03C9D7'
+          customFunc={() => handleClick('cart')}
+          color="blue"
+          icon={<BsChatLeft />}/>
+      </div>
     </div>
   )
 }
