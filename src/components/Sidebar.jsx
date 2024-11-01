cc613836d6e530c8e9760ab6462e71be2010ca93
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-01 16:19:35 +0000

    added close sidebar function

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,9 +8,15 @@ import { links } from '../data/dummy'
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const {activeMenu, setActiveMenu} = useStateContext();
+  const {activeMenu, setActiveMenu, screeenSize} = useStateContext();
   const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
+
+  const handleCloseSideBar = () => {
+    if (activeMenu && screeenSize <= 900) {
+      setActiveMenu(false)
+    }
+  }
 
   return (
     <div className='ml-3 h-screen
