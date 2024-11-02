a18f1f5b6d51cce52882b7debb4d0c4ed8fcfb75
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-02 13:11:50 +0000

    added icon to each card

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -35,7 +35,13 @@ const Ecommerce = () => {
             className='bg-white dark:text-gray-200
             dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl'
             >
-
+              <button type="button"
+              style={{ color: item.iconColor,
+                backgroundColor: item.iconBg
+              }}
+              >
+                {item.icon}
+              </button>
             </div>
           ))}
         </div>
