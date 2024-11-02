b546895e703d8346e934e595adf87e94ff383a7e
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-02 11:54:33 +0000

    added button

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -13,9 +13,17 @@ const Ecommerce = () => {
       rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className='flex justify-between items-center'>
             <div>
-              <p className='font-bold'>Earnings</p>
-              <p>£10.000</p>
+              <p className='font-bold text-gray-400'>Earnings</p>
+              <p className='text-2xl'>£10.000</p>
             </div>
+          </div>
+          <div className='mt-6'>
+            <Button
+              color="white"
+              bgColor="blue"
+              text="Download"
+              borderRadius="10px"
+            />
           </div>
         </div>
       </div>
