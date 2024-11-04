b2b1ad83a18ac232c3f59ac6c9b8d13a13020a00
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-04 15:00:09 +0000

    changed background to white

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -3,9 +3,21 @@ import { ChartComponent, SeriesCollectionDirective, SeriesDirective,
 Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
 import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
 
-const Stacked = () => {
+const Stacked = ({width, height}) => {
   return (
-    <div>Stacked</div>
+    <ChartComponent
+      width={width}
+      height={height}
+      id="stack chart"
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: 'white' }}
+    >
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
