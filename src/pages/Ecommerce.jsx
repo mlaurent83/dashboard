cb807ddb738ab29e85931bfdbdd51d3ad67f2fd0
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-04 14:53:43 +0000

    added imports to stacked chart

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -121,6 +121,12 @@ const Ecommerce = () => {
                     />
                   </div>
                 </div>
+                <div>
+                  <Stacked
+                  width="320px"
+                  height="360px"
+                  />
+                </div>
               </div>
             </div>
           </div>
