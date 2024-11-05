6ff6a855907272bfcd9867929ad874eff21122c6
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-05 11:52:50 +0000

    mapped through order grid

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import { GridComponent, ColumnDirective, ColumnsDirective, Resize, Sort, ContextMenu,
-Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
+Filter, Page, ExcelExport, PdfExport, Edit, Inject,
+Grid} from '@syncfusion/ej2-react-grids'
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy'
 import { Header } from '../components'
 
 const Orders = () => {
   return (
-    <div>
+    <div className='m-2 md:m-10 p-2 md:p-10
+    bg-white rounded-3xl'>
       <Header
       title="Orders"
       category="Page"
       />
+      <GridComponent>
+        <ColumnsDirective>
+        {ordersGrid.map((item, index)=>(
+          <ColumnDirective key={index} {...item}/>
+        ))}
+        </ColumnsDirective>
+      </GridComponent>
     </div>
   )
 }
