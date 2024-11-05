5516a99885256a4265b71a24566e606acefa2298
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-05 11:45:46 +0000

    added imports and header

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { GridComponent, ColumnDirective, ColumnsDirective, Resize, Sort, ContextMenu,
+Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy'
+import { Header } from '../components'
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div>
+      <Header
+      title="Orders"
+      category="Page"
+      />
+    </div>
   )
 }
 
