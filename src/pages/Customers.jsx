4019dd33b718801728f994e6a519bface3105cb9
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-05 12:13:37 +0000

    added customers page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { GridComponent, ColumnDirective, ColumnsDirective, Page, Selection,
+Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids'
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        toolbar={['Search']}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[ Page, Toolbar]} />
+      </GridComponent>
+    </div>
   )
 }
 
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -9,7 +9,7 @@ const Employees = () => {
   const editing = { allowDeleting: true, allowEditing: true };
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Orders" />
+      <Header category="Page" title="Employees" />
       <GridComponent
         dataSource={employeesData}
         allowPaging
