c7688be023ff38f35519ecf8fb88214e8170d17f
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-07 14:18:47 +0000

    added all imports to theme settings

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { MdOutlineCancel } from 'react-icons/md'
+import { BsCheck } from 'react-icons/bs'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 const ThemeSettings = () => {
   return (
