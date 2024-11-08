63df4518495d22351e965ee1ecabe1223df95c17
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-08 15:00:36 +0000

    added theme color in ecommerce page

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -6,6 +6,9 @@ import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropd
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Ecommerce = () => {
+
+  const {currentColor} = useStateContext();
+
   return (
     <div className='mt-24'>
       <div className="flex flex-wrap lg:flex-nowrap justify-center ">
@@ -21,7 +24,7 @@ const Ecommerce = () => {
           <div className='mt-6'>
             <Button
               color="white"
-              bgColor="blue"
+              bgColor={currentColor}
               text="Download"
               borderRadius="10px"
               size="md"
@@ -96,19 +99,19 @@ const Ecommerce = () => {
               </div>
               <div className='mt-5'>
                 <SparkLine
-                  currentColor="blue"
+                  currentColor={currentColor}
                   id="line-sparkline"
                   type="Line"
                   height="80px"
                   width="250px"
                   data={SparklineAreaData}
-                  color="blue"
+                  color={currentColor}
                 />
               </div>
               <div className='mt-10'>
                 <Button
                   color="white"
-                  bgColor="blue"
+                  bgColor={currentColor}
                   text="Download Report"
                   borderRadius="10px"
                 />
