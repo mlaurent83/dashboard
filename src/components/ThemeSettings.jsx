46879d5c48addf676d4d77c63cc42a75391857af
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-08 14:42:54 +0000

    added light and dark theme options

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -34,8 +34,8 @@ const ThemeSettings = () => {
             name="theme"
             value="Light"
             className='cursor-pointer'
-            onChange={() => {}}
-            checked={true}
+            onChange={setMode}
+            checked={currentMode === "Light"}
             />
             <label htmlFor='light' className='ml-2 text-md cursor-pointer'>
               Light
@@ -48,8 +48,8 @@ const ThemeSettings = () => {
             name="theme"
             value="Dark"
             className='cursor-pointer'
-            onChange={() => {}}
-            checked={true}
+            onChange={setMode}
+            checked={currentMode === "Dark"}
             />
             <label htmlFor='dark' className='ml-2 text-md cursor-pointer'>
               Dark
