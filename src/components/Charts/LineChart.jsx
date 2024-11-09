81f5481eb68dcbfa884606c6741875264fc87051
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-09 10:31:24 +0000

    added date into the chart

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective,
- Inject, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts'
+Inject, DateTime, Legend, Tooltip, LineSeries} from '@syncfusion/ej2-react-charts'
 import { lineCustomSeries, LinePrimaryYAxis, LinePrimaryXAxis } from '../../data/dummy'
 
 const LineChart = () => {
   return (
     <ChartComponent>
-
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]}/>
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) =>
+          <SeriesDirective key={index} {...item}/>
+        )}
+      </SeriesCollectionDirective>
     </ChartComponent>
   )
 }
