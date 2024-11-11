2cbfc0d70e32bd7cdfa2833b71e2c5810fa8293d
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-11 14:29:26 +0000

    rendered pie data

--- src/components/Charts/Pie.jsx
+++ src/components/Charts/Pie.jsx
@@ -2,28 +2,44 @@ import React from 'react'
 import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, AccumulationLegend, PieSeries, AccumulationDataLabel, Inject, AccumulationTooltip } from '@syncfusion/ej2-react-charts';
 import { useStateContext } from '../../contexts/ContextProvider'
 
-const LineChart = () => {
-
-  const {currentMode} = useStateContext();
+const Doughnut = ({ id, data, legendVisiblity, height }) => {
+  const { currentMode } = useStateContext();
 
   return (
-    <ChartComponent
-      id="line-chart"
-      height="420px"
-      primaryXAxis={LinePrimaryXAxis}
-      primaryYAxis={LinePrimaryYAxis}
-      chartArea={{border: {width: 0}}}
-      tooltip={{enable: true}}
+    <AccumulationChartComponent
+      id={id}
+      legendSettings={{ visible: legendVisiblity, background: 'white' }}
+      height={height}
       background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      tooltip={{ enable: true }}
     >
-      <Inject services={[LineSeries, DateTime, Legend, Tooltip]}/>
-      <SeriesCollectionDirective>
-        {lineCustomSeries.map((item, index) =>
-          <SeriesDirective key={index} {...item}/>
-        )}
-      </SeriesCollectionDirective>
-    </ChartComponent>
-  )
-}
+      <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]} />
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective
+          name="Sale"
+          dataSource={data}
+          xName="x"
+          yName="y"
+          innerRadius="40%"
+          startAngle={0}
+          endAngle={360}
+          radius="70%"
+          explode
+          explodeOffset="10%"
+          explodeIndex={2}
+          dataLabel={{
+            visible: true,
+            name: 'text',
+            position: 'Inside',
+            font: {
+              fontWeight: '600',
+              color: '#fff',
+            },
+          }}
+        />
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
+  );
+};
 
-export default LineChart
+export default Doughnut;
