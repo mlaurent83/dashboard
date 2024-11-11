dba48712d432fa9024121baed1c4df4361c65c3b
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-11 14:17:26 +0000

    displayed data in bar chart

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import {AxisModel, BarSeries, Category, ChartComponent, Inject, SeriesCollectionDirective, SeriesDirective} from'@syncfusion/ej2-react-charts';
+import {AxisModel, BarSeries, Category, ChartComponent, Inject, SeriesCollectionDirective, SeriesDirective,
+Legend, Tooltip, ColumnSeries, DataLabel } from'@syncfusion/ej2-react-charts';
 import { barCustomSeries, barPrimaryYAxis, barPrimaryXAxis} from '../../data/dummy'
 import { useStateContext } from '../../contexts/ContextProvider'
-import { Header } from '../../components'
+import { ChartsHeader } from '../../components'
 
 const Area = () => {
 
@@ -11,23 +12,23 @@ const Area = () => {
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white
     dark:bg-secondary-dark-bg rounded-3xl'>
-      <Header category="Area" title="Inflation Rate in Bar"/>
-    <ChartComponent
-      id="bar-chart"
-      height="420px"
-      primaryXAxis={barPrimaryXAxis}
-      primaryYAxis={barPrimaryYAxis}
-      chartArea={{border: {width: 0}}}
-      tooltip={{enable: true}}
-      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
-    >
-      <Inject services={[BarSeries, Category]}/>
-      <SeriesCollectionDirective>
-        {barCustomSeries.map((item, index) =>
-          <SeriesDirective key={index} {...item}/>
-        )}
-      </SeriesCollectionDirective>
-    </ChartComponent>
+      <ChartsHeader category="Bar" title="Olympic Medal Counts - RIO"/>
+      <ChartComponent
+  id="bar-chart"
+  height="420px"
+  primaryXAxis={barPrimaryXAxis}
+  primaryYAxis={barPrimaryYAxis}
+  chartArea={{ border: { width: 0 } }}
+  tooltip={{ enable: true }}
+  background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+>
+  <Inject services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]} />
+  <SeriesCollectionDirective>
+    {barCustomSeries.map((item, index) => (
+      <SeriesDirective key={index} {...item} />
+    ))}
+  </SeriesCollectionDirective>
+</ChartComponent>
     </div>
   )
 }
