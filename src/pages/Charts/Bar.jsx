778f904b2d6f568ca02af6bf70c8022f55aa1ae0
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-11-11 14:08:42 +0000

    displayed chart with no data

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import {AxisModel, BarSeries, Category, ChartComponent, Inject, SeriesCollectionDirective, SeriesDirective} from'@syncfusion/ej2-react-charts';
+import { barCustomSeries, barPrimaryYAxis, barPrimaryXAxis} from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
+import { Header } from '../../components'
 
-const Bar = () => {
+const Area = () => {
+
+  const {currentMode} = useStateContext();
+
   return (
-    <div>Bar</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white
+    dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category="Area" title="Inflation Rate in Bar"/>
+    <ChartComponent
+      id="bar-chart"
+      height="420px"
+      primaryXAxis={barPrimaryXAxis}
+      primaryYAxis={barPrimaryYAxis}
+      chartArea={{border: {width: 0}}}
+      tooltip={{enable: true}}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    >
+      <Inject services={[BarSeries, Category]}/>
+      <SeriesCollectionDirective>
+        {barCustomSeries.map((item, index) =>
+          <SeriesDirective key={index} {...item}/>
+        )}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+    </div>
   )
 }
 
-export default Bar
+export default Area
